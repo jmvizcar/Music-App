016987f1b0cc2f77bfba0c238f827c69c8cb8c7d
Jesus M. Vizcarra <jmvizcarra95@gmail.com>
2025-01-03 16:31:45 -0800

    Second Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MusicPlayer;
+
+public static class Program
+{
+  public static void Main()
+  {
+    Console.WriteLine("Hello World! I am alive!");
+  }
+}
