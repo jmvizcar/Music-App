ea47d56d6e1558159934bbcddca9862edb8856c9
Jesus M. Vizcarra <jmvizcarra95@gmail.com>
2025-01-05 19:10:54 -0800

    Add Play funcionality

--- MusicPlayer.cs
+++ MusicPlayer.cs
@@ -3,5 +3,16 @@ using System.Collections.Generic;
 
 public class MusicPlayer
 {
-  
+  public void Play(string songmp3){
+    using(var audioFile = new AudioFileReader(songmp3))
+    using(var outputDevice = new WaveOutEvent())
+    {
+      outputDevice.Init(audioFile);
+      outputDevice.Play();
+      while (outputDevice.PlaybackState == PlaybackState.Playing)
+      {
+        Thread.Sleep(1000);
+      }
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 {
   public static void Main()
   {
+    MusicPlayer MP = new MusicPlayer();
+    MP.Play(@"");
     Console.WriteLine("Hello World! I am alive!");
   }
 }
